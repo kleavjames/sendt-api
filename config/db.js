8b5d202392e27d8cd2c78d5c5cee5a53b1b86cdf
Kleavant James <james.olmedo@dottystylecreative.com>
2023-11-16 09:57:23 +0800

    point to production

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const connect = await mongoose.connect(process.env.MONGO_URI_DEV);
+    const connect = await mongoose.connect(process.env.MONGO_URI_PROD);
 
     console.log(`MongoDB connected: ${connect.connection.host}`);
 }
